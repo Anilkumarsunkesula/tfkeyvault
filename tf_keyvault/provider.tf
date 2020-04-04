4ec59d556154922ac5086df3fb16fc00e4e9e292
AnilSunkesula <anilsunkesula@gmail.com>
2020-04-04 17:56:23 +0530

    add templates

---
+++ tf_keyvault/provider.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+  features{}
+}
+
+# provider "azuread" {
+#   subscription_id = "${var.subscription_id}"
+#   client_id       = "${var.client_id}"
+#   client_secret   = "${var.client_secret}"
+#   tenant_id       = "${var.tenant_id}"
+# }
+
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "sa5d38"
+#     container_name       = "terraform"
+#     key                  = "terraform.tfstate"
+#     access_key           = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#   }
+# }
---
+++ tf_keyvault/terraform.tfvars
@@ -0,0 +1,6 @@
+subscription_id = ""
+client_id = ""
+client_secret = ""
+tenant_id = ""
+location = "West US 2"
+resource_group_name = "tflabrg"
